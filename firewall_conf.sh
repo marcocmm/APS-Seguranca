983ced027d4770196c0803b504d0b0b6d6cc97bb
marcocmm <marco.cmm@gmail.com>
2016-12-07 04:13:06 -0200

    k

--- firewall_conf.sh
+++ firewall_conf.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+iptables -P FORWARD ACCEPT
 
-./firewall_stop.sh
 ifconfig enp63s0:1 10.255.138.1/24
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 iptables -t nat -A POSTROUTING -s 10.255.138.2 -j SNAT --to 172.16.1.138
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3128 
+iptables -t nat -A PREROUTING -s 10.255.138.0/24 -p tcp --dport 80 -j REDIRECT --to-port 3128 
+iptables -t nat -A PREROUTING -s 10.255.138.0/24 -p tcp --dport 443 -j REDIRECT --to-port 3128 
 iptables -P FORWARD ACCEPT
 
 
@@ -19,10 +20,61 @@ iptables -P FORWARD ACCEPT
 
 #b) O firewall deve permitir que os hosts da Internet acessem o host 1 e host 2 via HTTP;
 #Como escolher qual dos hosts usar?
+iptables -t nat -A PREROUTING ! -s 10.255.138.0/24 -p tcp --dport 80 -j DNAT --to 10.255.138.2
+iptables -t nat -A PREROUTING ! -s 10.255.138.0/24 -p tcp --dport 81 -j DNAT --to 10.255.138.3:80
+iptables -A FORWARD -p tcp --dport 80 -j ACCEPT
+iptables -A FORWARD -p tcp --sport 80 -j ACCEPT
+
+
+
+#d) Somente o firewall pode acessar os hosts da LAN via SSH.
+iptables -A INPUT -s 10.255.138.0/24 -p tcp --dport 22 -j DROP
+
+#e) Somente um host da rede da UTFPR (exemplo do IP 172.16.2.X e não do IP 10.255.X.2) poderá acessar o firewall via SSH.
+iptables -A INPUT ! -s 172.16.1.137 -p tcp --dport 22 -j DROP
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Permite a comunicação através do firewall, alterando o ip para o do firewall
+iptables -t nat -A POSTROUTING -s 10.255.138.2 -j SNAT --to 172.16.1.1387
+#Faz todos pacotes http passarem pelo proxy
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3128 
+iptables -P FORWARD DROP
+
+#a) Deverá ser configurado um Proxy e um HIDS no firewall;
+
+#b) O firewall deve permitir que os hosts da Internet acessem o host 1 e host 2 via HTTP;
+#Como escolher qual dos hosts usar?
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to 10.255.138.2
 iptables -A FORWARD -p tcp --dport 80 -j ACCEPT
 iptables -A FORWARD -p tcp --sport 80 -j ACCEPT
 
+#c) Os hosts da LAN devem utilizar o proxy para acessar sites HTTP e essa configuração deve ser transparente (proxy transparente).
+#Arquivo confHIDS explica sobre a instalação e configuração do HIDS
+#Proxy
+#sudo dnf install squid
+#Substituir o arquivo squid.conf pelo enviado com o trabalho
 
 
 #d) Somente o firewall pode acessar os hosts da LAN via SSH.
